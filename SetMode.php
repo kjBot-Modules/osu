f11f137992ce5431f6ad4d9a6027ba72a7f6a5cb
kj415j45 <kj415j45@gmail.com>
2019-01-18 15:33:50 +0800

    Add tip

--- SetMode.php
+++ SetMode.php
@@ -10,7 +10,7 @@ class SetMode extends Module{
     public function process(array $args, MessageEvent $event): Message{
         $bind = Bind::GetID($event->getId());
         if($bind !== NULL){
-            if(DataStorage::SetData('osu.Bind.v1.mode/'.$event->getId(), Mode::Parse($args[1]??q('未指定模式'))))
+            if(DataStorage::SetData('osu.Bind.v1.mode/'.$event->getId(), Mode::Parse($args[1]??q('未指定模式'))??q('无法解析模式名')))
             return $event->sendBack('成功更改模式为 '.$args[1]);
             else q('更改模式失败，请联系master');
         }else{
